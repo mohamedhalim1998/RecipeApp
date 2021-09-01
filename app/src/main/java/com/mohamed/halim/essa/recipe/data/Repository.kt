8693fcd4bb9e0995e710bff77c3c11fb0d735c71
MohamedHalim <mohamedhalim1998@gmail.com>
2021-09-01 03:04:49 +0200

    EDIT: Handle no internet connection error

--- app/src/main/java/com/mohamed/halim/essa/recipe/data/Repository.kt
+++ app/src/main/java/com/mohamed/halim/essa/recipe/data/Repository.kt
@@ -8,15 +8,19 @@ import com.mohamed.halim.essa.recipe.data.network.model.DtoMapper
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flow
+import java.lang.Exception
 
 class Repository(private val networkSource: ApiService, private val localSource: RecipeDao) {
     private val dtoMapper = DtoMapper()
     private val entityMapper = EntityMapper()
     fun search(query: String): Flow<List<Recipe>> {
         return flow {
-            val recipes = dtoMapper.mapListToDomain(networkSource.search(query).recipes)
-            localSource.deleteAll()
-            localSource.insertList(entityMapper.mapListFromDomain(recipes))
+            try {
+                val recipes = dtoMapper.mapListToDomain(networkSource.search(query).recipes)
+                localSource.deleteAll()
+                localSource.insertList(entityMapper.mapListFromDomain(recipes))
+            } catch (e : Exception){
+            }
             localSource.getAll().collect {
                 emit(entityMapper.mapListToDomain(it))
             }
